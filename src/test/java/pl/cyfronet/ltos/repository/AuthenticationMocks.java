67678674ae1691ed545685fba06fd4a6dd5d9499
Piotr Sobczyk <piotrsobczyk7@gmail.com>
2016-07-19 08:34:31 +0200

    Dodanie użytkownikowi afiliacji

--- src/test/java/pl/cyfronet/ltos/repository/AuthenticationMocks.java
+++ src/test/java/pl/cyfronet/ltos/repository/AuthenticationMocks.java
@@ -7,27 +7,39 @@ import org.springframework.security.core.Authentication;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
 
+import pl.cyfronet.ltos.bean.Affiliation;
+import pl.cyfronet.ltos.bean.Role;
+import pl.cyfronet.ltos.bean.Team;
 import pl.cyfronet.ltos.bean.User;
 import pl.cyfronet.ltos.security.PortalUser;
 import pl.cyfronet.ltos.security.UserInfo;
 
 public class AuthenticationMocks {
 
-	private AuthenticationMocks() {
-	}
+    private AuthenticationMocks() {
+    }
 
-	public static Authentication userAuthentication(Long id) {
-		User user = User.builder().id(id).build();
-		List<GrantedAuthority> auth = Arrays.asList(new SimpleGrantedAuthority("ROLE_USER"));
-		PortalUser pu = PortalUser.builder().principal(UserInfo.fromUser(user)).user(user).authorities(auth).isAuthenticated(true).build();
-		return pu;
-	}
+    public static Authentication userAuthentication(Long id) {
+        Team team = new Team();
+        team.setName("test");
+        Affiliation affiliation = new Affiliation();
+        affiliation.setStatus("ACTIVE");
+        Role role = new Role();
+        role.setName("manager");
+        User user = User.builder().id(id).name("user").teams(Arrays.asList(team)).affiliations(Arrays.asList(affiliation)).roles(Arrays.asList(role)).build();
+        team.setMembers(Arrays.asList(user));
+        affiliation.setOwner(user);
+        role.setUsers(Arrays.asList(user));
+        List<GrantedAuthority> auth = Arrays.asList(new SimpleGrantedAuthority("ROLE_USER"));
+        PortalUser pu = PortalUser.builder().principal(UserInfo.fromUser(user)).user(user).authorities(auth).isAuthenticated(true).build();
+        return pu;
+    }
 
-	public static Authentication adminAuthentication(Long id) {
-		User user = User.builder().id(id).build();
-		List<GrantedAuthority> auth = Arrays.asList(new SimpleGrantedAuthority("ROLE_ADMIN"),new SimpleGrantedAuthority("ROLE_USER")); 
-		PortalUser pu = PortalUser.builder().principal(UserInfo.fromUser(user)).user(user).authorities(auth).isAuthenticated(true).build();
-		return pu;
-	}
-	
+    public static Authentication adminAuthentication(Long id) {
+        User user = User.builder().id(id).build();
+        List<GrantedAuthority> auth = Arrays.asList(new SimpleGrantedAuthority("ROLE_ADMIN"), new SimpleGrantedAuthority("ROLE_USER"));
+        PortalUser pu = PortalUser.builder().principal(UserInfo.fromUser(user)).user(user).authorities(auth).isAuthenticated(true).build();
+        return pu;
+    }
+
 }
