a7ae782805d9f5e8c8d991e9e77eccb2fc6ebfc1
bwilk <b.wilk@cyfronet.pl>
2016-08-08 17:44:29 +0200

    Improved HasAnyOfMetricsImplTest

--- src/test/java/pl/cyfronet/bazaar/HasAnyOfMetricsImplTest.java
+++ src/test/java/pl/cyfronet/bazaar/HasAnyOfMetricsImplTest.java
@@ -11,31 +11,43 @@ import pl.cyfronet.bazaar.engine.extension.constraint.action.impl.HasAnyOfMetric
 
 import com.agreemount.bean.document.Document;
 import com.agreemount.slaneg.action.ActionContext;
-import com.agreemount.slaneg.constraint.action.definition.ActionConstraint;
 import com.agreemount.slaneg.constraint.action.impl.QualifierImpl;
 
 
 public class HasAnyOfMetricsImplTest {
 
-    @Test
-    public void testDefinition() throws Exception {
+  @Test
+  public void testWithoutMetrics() throws Exception {
+
+      HasAnyOfMetricsImpl spy = Mockito.spy(new HasAnyOfMetricsImpl());
+      Mockito.mock(QualifierImpl.class);
+      HasAnyOfMetrics metrics = new HasAnyOfMetrics();        
+      Document document = new Document();
+      metrics.setMetrics(Arrays.asList("sampleMetric"));
+      ActionContext<Document> actionContext = new ActionContext<Document>(document);
+      Mockito.doReturn(metrics).when((QualifierImpl)spy).getConstraintDefinition();
+      Mockito.doReturn(actionContext).when((QualifierImpl)spy).getActionContext();
+      spy.isAvailable();
+  }
   
-        HasAnyOfMetricsImpl spy = Mockito.spy(new HasAnyOfMetricsImpl());
-        Mockito.mock(QualifierImpl.class);
-        HasAnyOfMetrics metrics = new HasAnyOfMetrics();        
-        Document document = new Document();
-        metrics.setMetrics(Arrays.asList("asdasda"));
-        //document.setMetrics(metrics2);
-        ActionContext<Document> actionContext = new ActionContext<Document>(document);
-        // Prevent/stub logic in super.method()
-        Mockito.doReturn(metrics).when((QualifierImpl)spy).getConstraintDefinition();
-        Mockito.doReturn(actionContext).when((QualifierImpl)spy).getActionContext();
-        // When
-        spy.isAvailable();
-     
-        // Then
-       // Mockito.verify(spy).load();
-        
-    }
+  @Test
+  public void testWithMetrics() throws Exception {
+
+      HasAnyOfMetricsImpl spy = Mockito.spy(new HasAnyOfMetricsImpl());
+      Mockito.mock(QualifierImpl.class);
+      HasAnyOfMetrics metrics = new HasAnyOfMetrics();        
+      Document document = new Document();
+      metrics.setMetrics(Arrays.asList("sampleMetric"));
+      //document.setMetrics(metrics2);
+      HashMap<String, Object> documentMetrics = new HashMap<String, Object>();
+      documentMetrics.put("sampleMetric", "sampleMetricValue");
+      document.setMetrics(documentMetrics);
+      ActionContext<Document> actionContext = new ActionContext<Document>(document);
+      // Prevent/stub logic in super.method()
+      Mockito.doReturn(metrics).when((QualifierImpl)spy).getConstraintDefinition();
+      Mockito.doReturn(actionContext).when((QualifierImpl)spy).getActionContext();
+      // When
+      spy.isAvailable();
+  }
 
 }
