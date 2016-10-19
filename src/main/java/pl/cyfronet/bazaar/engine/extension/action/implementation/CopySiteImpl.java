c3da62aa1c157014e4a177be5aecfb91d7226560
tomek <tomasz.szepieniec@gmail.com>
2016-10-19 23:58:35 +0200

    Fixing site with documents after action

--- src/main/java/pl/cyfronet/bazaar/engine/extension/action/implementation/CopySiteImpl.java
+++ src/main/java/pl/cyfronet/bazaar/engine/extension/action/implementation/CopySiteImpl.java
@@ -22,14 +22,24 @@ public class CopySiteImpl extends AbstractActionImpl<CopySite, IndigoDocument, A
         IndigoDocument from = (IndigoDocument)actionContext.getDocument(definition.getFromAlias());
         Preconditions.checkNotNull(from, "document was not found, alias [%s]", definition.getFromAlias());
 
-        IndigoDocument to = (IndigoDocument)actionContext.getDocument(definition.getToAlias());
+        Document to = actionContext.getDocument(definition.getToAlias());
         Preconditions.checkNotNull(to, "document was not found, alias [%s]", definition.getToAlias());
 
-        to.setSite(from.getSite());
+        //FIXME need support casting/copy
+        IndigoDocument indigoDocumentTo = new IndigoDocument();
+        indigoDocumentTo.setMetrics(to.getMetrics());
+        indigoDocumentTo.setAuthor(to.getAuthor());
+        indigoDocumentTo.setStates(to.getStates());
+        indigoDocumentTo.setId(to.getId());
+        indigoDocumentTo.setName(to.getName());
+        indigoDocumentTo.setRootUuid(to.getRootUuid());
+        indigoDocumentTo.setSlaUuid(to.getSlaUuid());
+        indigoDocumentTo.setIsLeaf(to.getIsLeaf());
+        indigoDocumentTo.setSite(from.getSite());
 
-        documentOperations.saveDocument(to);
+        documentOperations.saveDocument(indigoDocumentTo);
 
-        actionContext.addDocument(definition.getToAlias(), to);
+        actionContext.addDocument(definition.getToAlias(), indigoDocumentTo);
 
     }
 }
