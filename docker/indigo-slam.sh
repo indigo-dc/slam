0911f3d0859da49fa4a845895f365a1e1a603aab
Michał Szostak <szostak.m.f@gmail.com>
2016-10-20 11:34:37 +0200

    added db service start to startup script

--- docker/indigo-slam.sh
+++ docker/indigo-slam.sh
@@ -1,2 +1,4 @@
 #!/bin/bash
+service mongodb start
+service mysql start
 java -Dspring.cloud.config.username=developer -Dspring.cloud.config.password=developer $@ -jar /opt/indigo-slam/gs-accessing-data-rest-0.1.0.jar
