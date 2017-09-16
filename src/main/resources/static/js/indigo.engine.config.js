aba8fef482d9740684f3b4b5ffc2b6b1f259ed0c
tomek <tomasz.szepieniec@gmail.com>
2017-09-16 14:18:27 +0200

    introducing Storage SLA as another type of document

--- src/main/resources/static/js/indigo.engine.config.js
+++ src/main/resources/static/js/indigo.engine.config.js
@@ -7,11 +7,12 @@ app.config(function ($engineProvider, SESSION) {
     var slaSpec = {
         documentJSON: {
             "states": {
-                "documentType": "sla"
+                "documentType": "sla",
+                "serviceType": "computing"
             },
             "metrics": {}
         },
-        name: 'sla',
+        name: 'Computing SLA',
         list: {
             columns: [
                 {name: '@index', type: 'link', caption: 'ID'},
@@ -34,13 +35,53 @@ app.config(function ($engineProvider, SESSION) {
                 }
             ],
             details: {
-                'caption': 'SLA'
+                'caption': 'Computing SLA'
             },
             showValidationButton: true,
             summary: true
         }
     };
     $engineProvider.document('sla', '/sla', '/sla/:id', ['SignedSlaComp'], slaSpec);
+
+    var slaSSpec = {
+        documentJSON: {
+            "states": {
+                "documentType": "sla",
+                "serviceType": "storage"
+            },
+            "metrics": {}
+        },
+        name: 'Storage SLA',
+        list: {
+            columns: [
+                {name: '@index', type: 'link', caption: 'ID'},
+                {name: 'name', caption: 'Name'},
+                {name: 'siteName', caption: 'site'},
+                {name: 'createdAt', type: 'date', caption: 'created'},
+                {name: 'states.mainState', caption: 'status'},
+                {name: 'metrics.startComp', caption: 'Start', type: 'date'},
+                {name: 'metrics.endComp', caption: 'End', type: 'date'},
+                // {name: 'discipline', caption: 'disciplne'},
+                // {name: 'proposalEvaluation', condition: "states.documentState == 'evaluated"}
+            ],
+            caption: 'Storage SLAs'
+        },
+        document: {
+            steps: [
+                {
+                    name: 'GENERAL',
+                    categories: 'mcComputing'
+                }
+            ],
+            details: {
+                'caption': 'Storage SLA'
+            },
+            showValidationButton: true,
+            summary: true
+        }
+    };
+    $engineProvider.document('slaS', '/slaS', '/slaS/:id', ['SignedSlaComp'], slaSSpec);
+
 
     $engineProvider.dashboard({url: '/dashboard', label: 'SLAs', activetab: 'dashboard'},
         [
@@ -51,9 +92,23 @@ app.config(function ($engineProvider, SESSION) {
                 showCreateButton: false
             },
             {
-                queryId: 'workingSla',
-                label: 'SLAs in progrees',
+                queryId: 'SignedSlaStorage',
+                label: 'My signed storage SLAs',
                 documentModelId: 'sla',
+                showCreateButton: false
+            },
+            {
+                queryId: 'workingSla',
+                label: 'Computing SLAs in progrees',
+                documentModelId: 'sla',
+                showCreateButton: true,
+                controller: 'indigoDocumentListCtrl',
+                contentTemplateUrl: '/js/engine/indigoDocumentList.tpl.html'
+            },
+            {
+                queryId: 'workingSlaS',
+                label: 'Storage SLAs in progrees',
+                documentModelId: 'slaS',
                 showCreateButton: true,
                 controller: 'indigoDocumentListCtrl',
                 contentTemplateUrl: '/js/engine/indigoDocumentList.tpl.html'
