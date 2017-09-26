ddb9b2239eff5c5c7eca22f4087db46c0938fc7b
Jakub Sawicki <ymsawick@cyfronet.pl>
2017-09-26 09:47:58 +0200

    Correct the tests of CmdbOwnerService

--- src/test/java/pl/cyfronet/ltos/security/CmdbOwnerServiceTest.java
+++ src/test/java/pl/cyfronet/ltos/security/CmdbOwnerServiceTest.java
@@ -5,14 +5,10 @@ import org.json.JSONObject;
 import org.junit.Test;
 import pl.cyfronet.ltos.repository.CmdbRepository;
 
-import java.util.HashMap;
-import java.util.Map;
 import java.util.Set;
 
 import static org.junit.Assert.assertThat;
-import static org.mockito.Mockito.mock;
-import static org.mockito.Mockito.verify;
-import static org.mockito.Mockito.when;
+import static org.mockito.Mockito.*;
 import static org.mockito.internal.verification.VerificationModeFactory.times;
 
 public class CmdbOwnerServiceTest {
@@ -46,10 +42,7 @@ public class CmdbOwnerServiceTest {
         service.getOwnedProviders("2@owner");
         service.getOwnedProviders("1@owner");
 
-        verify(cmdbRepository, times(1)).get("provider");
-        verify(cmdbRepository, times(1)).getById("provider", "provider1");
-        verify(cmdbRepository, times(1)).getById("provider", "provider2");
-        verify(cmdbRepository, times(1)).getById("provider", "provider3");
+        verify(cmdbRepository, times(1)).get("provider", "include_docs=true");
     }
 
     @Test
@@ -68,37 +61,30 @@ public class CmdbOwnerServiceTest {
         Set<String> providers2 = service.getOwnedProviders("2@owner");
         assertThat(providers2, Matchers.containsInAnyOrder("provider1", "provider2", "provider3"));
 
-        verify(cmdbRepository, times(2)).get("provider");
-        verify(cmdbRepository, times(2)).getById("provider", "provider1");
-        verify(cmdbRepository, times(2)).getById("provider", "provider2");
-        verify(cmdbRepository, times(2)).getById("provider", "provider3");
+        verify(cmdbRepository, times(2)).get("provider", "include_docs=true");
     }
 
     private CmdbRepository mockCmdbRepository() {
         CmdbRepository cmdbRepository = mock(CmdbRepository.class);
-        String repoProvidersReturn = "{\"rows\":[{\"id\":\"provider1\"},{\"id\":\"provider2\"},{\"id\":\"provider3\"}]}";
-        Map<String, JSONObject[]> providerByIdReturns = new HashMap<>();
-        providerByIdReturns.put("provider1", new JSONObject[]{
-                new JSONObject("{\"data\":{\"owners\":[\"1@owner\",\"2@owner\"]}}")
-        });
-        providerByIdReturns.put("provider2", new JSONObject[]{
-                new JSONObject("{\"data\":{\"owners\":[\"1@owner\",\"3@owner\"]}}"),
-                new JSONObject("{\"data\":{\"owners\":[\"2@owner\",\"3@owner\"]}}")
-        });
-        providerByIdReturns.put("provider3", new JSONObject[]{
-                new JSONObject("{\"data\":{\"owners\":[\"1@owner\",\"2@owner\",\"3@owner\"]}}")
-        });
+        String provider1 = "{\"data\":{\"owners\":[\"1@owner\",\"2@owner\"]}}";
+        String provider2inv1 = "{\"data\":{\"owners\":[\"1@owner\",\"3@owner\"]}}";
+        String provider2inv2 = "{\"data\":{\"owners\":[\"2@owner\",\"3@owner\"]}}";
+        String provider3 = "{\"data\":{\"owners\":[\"1@owner\",\"2@owner\",\"3@owner\"]}}";
 
-        when(cmdbRepository.get("provider")).thenReturn(new JSONObject(repoProvidersReturn));
-        when(cmdbRepository.getById("provider", "provider1")).thenReturn(
-                new JSONObject("{\"data\":{\"owners\":[\"1@owner\",\"2@owner\"]}}")
-        );
-        when(cmdbRepository.getById("provider", "provider2")).thenReturn(
-                new JSONObject("{\"data\":{\"owners\":[\"1@owner\",\"3@owner\"]}}"),
-                new JSONObject("{\"data\":{\"owners\":[\"2@owner\",\"3@owner\"]}}")
-        );
-        when(cmdbRepository.getById("provider", "provider3")).thenReturn(
-                new JSONObject("{\"data\":{\"owners\":[\"1@owner\",\"2@owner\",\"3@owner\"]}}")
+        String repoProvidersReturn1 = "{\"rows\":[" +
+                "{\"id\":\"provider1\",\"doc\":"+provider1+"}," +
+                "{\"id\":\"provider2\",\"doc\":"+provider2inv1+"}," +
+                "{\"id\":\"provider3\",\"doc\":"+provider3+"}" +
+            "]}";
+        String repoProvidersReturn2 = "{\"rows\":[" +
+                "{\"id\":\"provider1\",\"doc\":"+provider1+"}," +
+                "{\"id\":\"provider2\",\"doc\":"+provider2inv2+"}," +
+                "{\"id\":\"provider3\",\"doc\":"+provider3+"}" +
+            "]}";
+
+        when(cmdbRepository.get("provider", "include_docs=true")).thenReturn(
+                new JSONObject(repoProvidersReturn1),
+                new JSONObject(repoProvidersReturn2)
         );
         return cmdbRepository;
     }
