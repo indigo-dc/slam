9e5f3a45cec9bf76089486346e489e6fb60a4a55
Krzysztof Marszałek <marszalekk@gmail.com>
2018-04-03 11:17:07 +0200

    add jar version to the footer

--- src/main/java/pl/cyfronet/ltos/controller/IndexController.java
+++ src/main/java/pl/cyfronet/ltos/controller/IndexController.java
@@ -2,10 +2,8 @@ package pl.cyfronet.ltos.controller;
 
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
-import pl.cyfronet.ltos.bean.User;
 import pl.cyfronet.ltos.security.PortalUser;
 
 import java.util.ArrayList;
@@ -17,7 +15,7 @@ public class IndexController {
     public ModelAndView home(PortalUser user) {
         ArrayList<String> roles = new ArrayList<>();
 
-        for(GrantedAuthority authority : user.getAuthorities()) {
+        for (GrantedAuthority authority : user.getAuthorities()) {
             roles.add(authority.getAuthority());
         }
 
@@ -25,6 +23,11 @@ public class IndexController {
         mav.addObject("user", user);
         mav.addObject("user_roles", roles);
         mav.addObject("admin_sites", roles);
+        String version  = System.getProperty("app.version");
+        if (version==null){
+            version = String.class.getPackage().getImplementationVersion();
+        }
+        mav.getModelMap().addAttribute("version", version);
         return mav;
     }
 }
