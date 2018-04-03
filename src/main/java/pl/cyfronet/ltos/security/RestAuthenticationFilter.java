478c18e802f4bd505d35e03a83dc50f62e7e7351
Marek Kasztelnik <mkasztelnik@gmail.com>
2018-04-03 10:47:51 +0200

    Fix rest API authentication filter
    `OncePerRequestFilter` filter should be used instead of
    `AbstractAuthenticationProcessingFilter` (the last one make redirect at
    the end to `/`).

--- src/main/java/pl/cyfronet/ltos/security/RestAuthenticationFilter.java
+++ src/main/java/pl/cyfronet/ltos/security/RestAuthenticationFilter.java
@@ -1,21 +1,24 @@
 package pl.cyfronet.ltos.security;
 
-import lombok.extern.log4j.Log4j;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.security.core.Authentication;
-import org.springframework.security.core.AuthenticationException;
-import org.springframework.security.oauth2.client.OAuth2ClientContext;
-import org.springframework.security.oauth2.common.DefaultOAuth2AccessToken;
-import org.springframework.security.oauth2.provider.authentication.BearerTokenExtractor;
-import org.springframework.security.web.authentication.AbstractAuthenticationProcessingFilter;
+import java.io.IOException;
 
+import javax.servlet.FilterChain;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import java.io.IOException;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.oauth2.client.OAuth2ClientContext;
+import org.springframework.security.oauth2.common.DefaultOAuth2AccessToken;
+import org.springframework.security.oauth2.provider.authentication.BearerTokenExtractor;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import lombok.extern.log4j.Log4j;
 
 @Log4j
-public class RestAuthenticationFilter extends AbstractAuthenticationProcessingFilter {
+public class RestAuthenticationFilter extends OncePerRequestFilter {
 
     @Autowired
     private OAuth2ClientContext context;
@@ -25,27 +28,24 @@ public class RestAuthenticationFilter extends AbstractAuthenticationProcessingFi
 
     private BearerTokenExtractor extractor = new BearerTokenExtractor();
 
-    public RestAuthenticationFilter(String defaultFilterProcessesUrl) {
-        super(defaultFilterProcessesUrl);
-
-        setAuthenticationManager(authentication -> authentication);
+    @Override
+    public void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
+        tryToAuthorize(request);
+        filterChain.doFilter(request, response);
     }
 
-    @Override
-    public Authentication attemptAuthentication(HttpServletRequest request, HttpServletResponse response) throws AuthenticationException, IOException, ServletException {
+    private void tryToAuthorize(HttpServletRequest request) {
         try {
             Authentication extract = extractor.extract(request);
             if (extract != null) {
                 String jwt = extract.getPrincipal().toString();
                 context.setAccessToken(new DefaultOAuth2AccessToken(jwt));
 
-                return authenticationService.getPortalUser();
+                SecurityContextHolder.getContext().setAuthentication(authenticationService.getPortalUser());
             }
-        } catch(Exception e) {
+        }  catch(Exception e) {
             log.info("Unable to authenticate to rest API - wrong token", e);
         }
-
-        return null;
     }
 }
 
